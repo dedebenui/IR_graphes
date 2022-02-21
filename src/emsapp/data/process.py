0d86676b62ed25c8e886c7bb111b66542d6fa72b
Benoît Sierro <benoit.sierro@fr.ch>
2022-02-21 15:05:37 +0100

    transformers implemented

--- src/emsapp/data/process.py
+++ src/emsapp/data/process.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from emsapp.config import Config, ProcessConfig
 from emsapp.data import DataSet
 
 from emsapp.data.filters import Filter
@@ -12,12 +13,21 @@ from emsapp.data.transformers import Transformer
 class Process:
     filter: Filter
     splitter: Splitter
-    transformers: list[Transformer]
+    transformers: dict[str, Transformer]
     grouper: Grouper
 
-    def __call__(self, raw_entries:Entries)->list[DataSet]:
-        filtered_entries = self.filter(raw_entries)
+    @classmethod
+    def from_config(cls, config: ProcessConfig):
+        return cls()
+
+    def __call__(self, raw_entries: Entries) -> list[DataSet]:
+        filtered_entries = Entries([e for e in raw_entries if self.filter(e)], "filtered")
         splitted = self.splitter(filtered_entries)
-        final_data = [trans(entries) for entries in splitted for trans in self.transformers]
+        final_data = [
+            trans(entries) for entries in splitted for trans in self.transformers.values()
+        ]
         return self.grouper(final_data)
 
+
+def current_process() -> list[Process]:
+    config = Config().process
