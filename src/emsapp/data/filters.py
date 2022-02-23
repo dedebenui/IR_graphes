0efe4eaa4359c4fc1012d312de405b27ec77c881
Benoît Sierro <benoit.sierro@fr.ch>
2022-02-23 13:02:25 +0100

    Revamping process description and flow

--- src/emsapp/data/filters.py
+++ src/emsapp/data/filters.py
@@ -1,66 +1,60 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from emsapp.config import Config
+from typing import Iterable
+from emsapp.config import Config, FilterConfig
 from emsapp.data.loading import Entries, Entry
+from emsapp.i18n import _
+
+registered: dict[str, type[Filter]] = {}
 
 
 class Filter(ABC):
+    @classmethod
+    def create(conf: FilterConfig) -> Filter:
+        cls = registered.get(conf.type)
+        if cls:
+            return cls(conf)
+
+        raise ValueError(_("Invalid filter specifications : {conf!r}").format(conf=conf))
+
+    @abstractmethod
+    def __init__(self, conf: FilterConfig):
+        ...
+
     @abstractmethod
     def __call__(self, entry: Entry) -> bool:
         ...
 
-    def __and__(self, other: Filter) -> CombinedAndFilter:
-        return CombinedAndFilter(self, other)
 
-    def __or__(self, other: Filter) -> CombinedOrFilter:
-        return CombinedOrFilter(self, other)
-
-
-class CombinedFilter(Filter):
-    def __init__(self, filter_a: Filter, filter_b: Filter):
-        self.filter_a = filter_a
-        self.filter_b = filter_b
-
-
-class CombinedAndFilter(CombinedFilter):
-    def __call__(self, entry: Entry) -> bool:
-        return self.filter_a(entry) and self.filter_b(entry)
-
-
-class CombinedOrFilter(CombinedFilter):
-    def __call__(self, entry: Entry) -> bool:
-        return self.filter_a(entry) or self.filter_b(entry)
-
-
-class NullFilter(Filter):
-    def __call__(self, entry: Entry) -> bool:
-        return True
-
-
-class NotFilter(Filter):
-    def __init__(self, filter: Filter):
-        self.filter = filter
-
-    def __call__(self, entry: Entry) -> bool:
-        return not self.filter(entry)
-
-
-class ColumnFilter(Filter):
-    def __init__(self, column: str, value: str):
-        self.col = column
-        self.val = value
-        if column not in Entry.fields():
+class IncludeFilter(Filter):
+    def __init__(self, conf: FilterConfig):
+        self.col = conf.column
+        self.val = set(conf.values)
+        if conf.column not in Entry.fields():
             raise ValueError(f"Can only filter existing columns : {Entry.fields()!r}")
 
     def __call__(self, entry: Entry) -> bool:
-        return getattr(entry, self.col) == self.val
+        return getattr(entry, self.col) in self.val
 
 
-def create_filter(type: str = None, column: str = None, value: str = None) -> Filter:
-    if not type:
-        return NullFilter()
-    if type == "include":
-        return ColumnFilter(column, value)
+class ExcludeFilter(Filter):
+    def __init__(self, conf: FilterConfig):
+        self.col = conf.column
+        self.val = set(conf.values)
+        if conf.column not in Entry.fields():
+            raise ValueError(f"Can only filter existing columns : {Entry.fields()!r}")
 
-    raise ValueError("Invalid filter specifications")
+    def __call__(self, entry: Entry) -> bool:
+        return getattr(entry, self.col) not in self.val
+
+
+class ExcludeFilter(Filter):
+    def __init__(self, conf: FilterConfig):
+        self.col = conf.column
+        self.val = set(conf.values)
+        if conf.column not in Entry.fields():
+            raise ValueError(f"Can only filter existing columns : {Entry.fields()!r}")
+
+    def __call__(self, entry: Entry) -> bool:
+        return getattr(entry, self.col) not in self.val
